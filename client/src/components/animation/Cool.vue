7394b0af6b370791914b2d5a7b20175c540a0013
kaiyan99yankai <kaiyan99yankai@gmail.com>
2021-07-30 11:59:54 +0800

    7/30

--- client/src/components/animation/Cool.vue
+++ client/src/components/animation/Cool.vue
@@ -1,13 +1,15 @@
 <template>
-  <v-vanta effect="rings" :options=options></v-vanta>
+  <v-vanta ref="vanta" effect="rings" :options=options :key="refresh"></v-vanta>
 </template>
 
 <script>
 import VVanta from 'vue-vanta/src/Vanta.vue';
+import {GUI} from '../../../node_modules/three/examples/jsm/libs/dat.gui.module.js';
 export default {
   components: { VVanta },
   data () {
     return {
+	  refresh: true,
       options: {
         mouseControls: true,
         touchControls: true,
@@ -15,9 +17,50 @@ export default {
         minHeight: 880.00,
         minWidth: 200.00,
         scale: 1.00,
-        scaleMobile: 1.00
+        scaleMobile: 1.00,
+		backgroundAlpha: 1.00
       }
     }
-  }
+  },
+  methods: {
+    init: function (){
+      let that = this;
+      var controls = new function(){
+        this.alpha = 1.00;
+      }
+      var gui = new GUI();
+      gui.add(controls, 'alpha', 0, 1);
+      function copyObj(obj){
+        var newobj = {};
+        for ( var key in obj) {
+          newobj[key] = obj[key ];
+        }
+        return newobj;
+      }
+      that.$refs.vanta.options.backgroundAlpha=controls.alpha;
+      var old_options = copyObj(this.$refs.vanta.options);
+      var tick = 0;
+      function animate(){
+        tick+=1;
+        requestAnimationFrame(animate);
+        that.$refs.vanta.options.backgroundAlpha=controls.alpha;
+        if(tick%100===0){
+          console.log(tick);
+          if(old_options.backgroundAlpha!==that.$refs.vanta.options.backgroundAlpha){
+            console.log("变化");
+            that.refresh=!that.refresh;
+            //that.$destroy();
+            //that.$refs.vanta.$forceUpdate();
+            //location.reload();
+            old_options = copyObj(that.$refs.vanta.options);
+          }
+        }
+      }
+      animate();
+    }
+  },
+  mounted(){
+    this.init();
+  }	
 }
 </script>
--- client/src/components/animation/Dreamlike.vue
+++ client/src/components/animation/Dreamlike.vue
@@ -1,13 +1,15 @@
 <template>
-  <v-vanta effect="halo" :options=options></v-vanta>
+  <v-vanta ref="vanta" effect="halo" :options=options :key="refresh"></v-vanta>
 </template>
 
 <script>
 import VVanta from 'vue-vanta/src/Vanta.vue';
+import {GUI} from '../../../node_modules/three/examples/jsm/libs/dat.gui.module.js'
 export default {
   components: { VVanta },
   data () {
     return {
+	  refresh: true,
       options: {
         mouseControls: true,
         touchControls: true,
@@ -20,6 +22,51 @@ export default {
         size: 1.60
       }
     }
+  },
+  methods: {
+    init: function (){
+      let that = this;
+      var controls = new function(){
+        this.size = 1.60;
+        this.amplitude = 1.50;
+      }
+      var gui = new GUI();
+      gui.add(controls, 'size', 0.1, 3);
+      gui.add(controls, 'amplitude', 0, 3);
+      function copyObj(obj){
+        var newobj = {};
+        for ( var key in obj) {
+          newobj[key] = obj[key ];
+        }
+        return newobj;
+      }
+      that.$refs.vanta.options.size=controls.size;
+      that.$refs.vanta.options.amplitudeFactor=controls.amplitude;
+      var old_options = copyObj(this.$refs.vanta.options);
+      var tick = 0;
+      function animate(){
+        tick+=1;
+        requestAnimationFrame(animate);
+        that.$refs.vanta.options.size=controls.size;
+        that.$refs.vanta.options.amplitudeFactor=controls.amplitude;
+        if(tick%100===0){
+          console.log(tick);
+          if(old_options.size!==that.$refs.vanta.options.size||old_options.amplitudeFactor!==that.$refs.vanta.options.amplitudeFactor){
+            console.log("变化");
+            that.refresh=!that.refresh;
+            //that.$destroy();
+            //that.$refs.vanta.$forceUpdate();
+            //location.reload();
+            old_options = copyObj(that.$refs.vanta.options);
+          }
+        }
+      }
+      animate();
+    }
+  },
+  mounted(){
+    this.init();
   }
+
 }
 </script>
